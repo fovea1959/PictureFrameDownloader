ddb34a3317527f8cac8fdd661583ff1507e071e3
Doug Wegscheid <dwegscheid@sbcglobal.net>
2025-12-27 13:21:09 -0500

    Need to know where scripts are!

--- download_and_process.sh
+++ download_and_process.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+echo "Script directory: ${SCRIPT_DIR}"
+
 REMOTE="googledrive:Bettys Frame Pictures"
 PIX_RAW=../pix-raw/
 SCRATCH=/tmp/betty/
@@ -10,6 +13,6 @@ mkdir -p "$SCRATCH" "$PIX_SCRATCH"
 rclone sync --log-level=INFO --log-file="$SCRATCH/sync.log" --use-json-log "$REMOTE" "$PIX_RAW"
 rclone lsjson "$REMOTE" --recursive --metadata > "$SCRATCH/remote.json"
 rsync -r -vv --checksum --delete "$PIX_RAW" "$PIX_SCRATCH"
-python ./ProcessRclonedFiles.py --json="$SCRATCH/remote.json" --dir="$PIX_SCRATCH"
+python $SCRIPT_DIR/ProcessRclonedFiles.py --json="$SCRATCH/remote.json" --dir="$PIX_SCRATCH"
 rsync -r -vv --checksum --itemize-changes --delete "$PIX_SCRATCH" "$PIX_FINAL"
 rm -rf "$PIX_SCRATCH"
