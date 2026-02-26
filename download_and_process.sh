73b6a6b2e54577426da9b192d0e01eabea60bf13
Doug Wegscheid <dwegscheid@sbcglobal.net>
2026-02-26 12:56:56 -0600

    iSave.

--- download_and_process.sh
+++ download_and_process.sh
@@ -4,13 +4,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 echo "Script directory: ${SCRIPT_DIR}"
 
 REMOTE="googledrive:Bettys Frame Pictures"
-PIX_RAW=../pix-raw/
+PIX_RAW=$HOME/pix-raw/
 SCRATCH=/tmp/betty/
 PIX_SCRATCH=$SCRATCH/$$/
-PIX_FINAL=../pix-final/
+PIX_FINAL=$HOME/Pictures/google/
 
 mkdir -p "$SCRATCH" "$PIX_SCRATCH"
-rclone sync --log-level=INFO --log-file="$SCRATCH/sync.log" --use-json-log "$REMOTE" "$PIX_RAW"
+rclone sync --log-level=DEBUG --log-file="$SCRATCH/sync.log" --use-json-log "$REMOTE" "$PIX_RAW"
 rclone lsjson "$REMOTE" --recursive --metadata > "$SCRATCH/remote.json"
 rsync -r -vv --checksum --delete "$PIX_RAW" "$PIX_SCRATCH"
 python $SCRIPT_DIR/ProcessRclonedFiles.py --json="$SCRATCH/remote.json" --dir="$PIX_SCRATCH"
